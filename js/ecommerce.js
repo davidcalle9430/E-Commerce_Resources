4ecf162ee8c9674ba53c06d7ba0cdd2930a72049
Fabian <merchanf@javeriana.edu.co>
2015-10-01 18:34:09 -0500

    product view implemented without add to shopping cart requests

--- js/ecommerce.js
+++ js/ecommerce.js
@@ -3,13 +3,8 @@
 */
 /* product preview */
 $(".thumbnail").click(function() {
-
-    var img = document.getElementsByClassName('thumbnail');
-    console.log(img);
-    var style = img.currentStyle || window.getComputedStyle(img, false);
-    var url = style.backgroundImage.slice(4, -1);
-        url = style.backgroundImage.slice(4, -1).replace(/"/g, "");
-    console.log("url = "+url);
+    var url = $(this).css('background-image');
+        url = url.replace('url(','').replace(')','');
     $("#main_photo").attr("src", url );
 });
 /* Quantity bar*/
@@ -21,4 +16,17 @@ $("#minus-button").click(function() {
     var quantity = document.getElementById('quantity_input');
     if( quantity.value > 1 )
       quantity.value--;
+});
+/*Add to shopping cart request*/
+$("#add-to-shopping-cart-button").click(function() {
+    $.ajax({
+      method: "GET",
+      url: "TODO",
+      data: {
+          //To do
+      }
+    })
+      .done(function( msg ) {
+        alert( "Data Saved: " + msg );
+      });
 });
