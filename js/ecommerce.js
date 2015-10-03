9a38b2e61d8258f1ef43337e7f307e619133385e
Fabian <merchanf@javeriana.edu.co>
2015-10-03 17:19:42 -0500

    product view almost finished without requests

--- js/ecommerce.js
+++ js/ecommerce.js
@@ -2,6 +2,9 @@
 *   Product view
 */
 /* product preview */
+var quantity = 1;
+var size = null;
+var color = null;
 $(".thumbnail").click(function() {
     var url = $(this).css('background-image');
         url = url.replace('url(','').replace(')','');
@@ -9,16 +12,34 @@ $(".thumbnail").click(function() {
 });
 /* Quantity bar*/
 $("#plus-button").click(function() {
-    var quantity = document.getElementById('quantity_input');
+    var quantity = document.getElementById('quantity-input');
     quantity.value++;
 });
 $("#minus-button").click(function() {
-    var quantity = document.getElementById('quantity_input');
+    var quantity = document.getElementById('quantity-input');
     if( quantity.value > 1 )
       quantity.value--;
 });
-/*Add to shopping cart request*/
+// Obtener detalles del producto
+var selectedSize  = null;
+$(".size-item").click(function() {
+    size = $(this).text();
+});
+$(".color-item").click(function() {
+    color = $(this).text();
+});
+function getQuantity(){
+    var regex = /^\d+$/g;
+    quantity = $('#quantity-input').val();
+    if( !regex.test(quantity) )
+        alert("Por favor ingrese una cantidad correcta");
+    else if( quantity < 1 )
+        alert("Por favor ingrese una cantidad correcta");
+}
+
+// Solicitud para añador al carrito de compras
 $("#add-to-shopping-cart-button").click(function() {
+    getQuantity();
     $.ajax({
       method: "GET",
       url: "TODO",
--- js/setMainPicture.js
+++
@@ -1,4 +0,0 @@
-$(".thumbnail").click(function() {
-    var sourcee = $(this).find("img").attr("src");
-    $("#main_photo").attr("src", sourcee );
-});
